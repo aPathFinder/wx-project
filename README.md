1415e8e4dae975f8e93fa2371b1f4a2cc542cfaa
aPathFinder Guo <gpfcodejob@gmail.com>
2020-03-09 14:58:38 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,76 @@
+# wx-mini
+
+> 此项目是基于 wx-mini 项目,做的修改. 本项目用于公司生产环境的开发.如果有好的idea/issue,希望提issue,我将会尽快解决,也将会帮助公司完善项目.
+[源项目](https://github.com/webpersonalserver/wx-mini)
+
+**升级内容**
+1. 从babel6 升级到了 babael7;
+
+---
+> 以下为源项目说明; 不曾做过修改;
+
+使用 `webpack`, `babel`, `scss` 开发的微信小程序项目脚手架
+
+## 本项目重要开发工具本版说明
+
+* `webpack 3.8.1`
+* `yarn 1.9.4`
+* `node 8.11.1`
+* `npm 5.6.0`
+* `gulp 3.9.1`
+
+## 功能
+
+* 支持引用 `node_modules` 模块
+* 支持通过配置 `alias` 来避免 `../../../` 之类的模块引用
+* 通过 `babel` 支持更丰富的 `ES6` 兼容，包括 `async/await`
+* 内置 `promise` 和 `lodash`（`lodash` 按需引入相应模块，不会全部引入）
+* 使用 `scss` 编写 `.wxss` 文件，内置了一些有用的 `mixins` 和 `extends`
+* 提供 `ENVIRONMENT` 和 `process.env.NODE_ENV` 全局常量辅助开发
+* 支持 `eslint` 基础代码检测
+
+## 开始使用
+
+确保安装了 [Node.js](https://nodejs.org/) (>= `v4.2`) 和 [yarn](https://yarnpkg.com) 或 [npm](https://www.npmjs.com/package/npm)
+
+1.  `git clone` 此项目
+2.  通过命令行工具 `cd` 到这个目录，执行`npm install` 或 `yarn` 安装依赖模块
+3.  执行 `npm run dev`或`yarn dev` 开始开发
+4.  通过微信开发者工具，添加 `dist` 目录到项目上
+
+## 内置命令
+
+* 执行命令  `npm run dev`或`yarn dev` 运行项目（不带有压缩项目的功能）
+* 执行命令  `npm run build`或`yarn build` 生产编译打包项目（不带有压缩项目的功能）
+* 执行命令  `npm run minify`或`yarn minify` 生产编译压缩打包项目（带有压缩项目的功能）
+
+# 项目目录结构说明
+``````
+wx-mini
+|---------dist                            项目编译打包后的文件
+|---------src                             项目开发文件
+          |-----------components                      开发组件文件
+          |-----------images                          项目所用图标或图片文件
+          |-----------pages                           页面开发文件
+                      |--------index                      首页
+                               |------index.js                首页js
+                               |------index.json              首页json
+                               |------index.scss              首页scss
+                               |------index.xml               首页xml
+          |-----------sass                            项目全局或公用scss文件
+          |-----------utils                           项目公用方法或工具类方法
+          |-----------wxs                             项目公用过滤器
+          |-----------app.js                          小程序主js文件
+          |-----------app.json                        小程序主json配置文件
+          |-----------app.wxss                        小程序主wxss样式文件
+|---------gulpfile.js                     项目压缩的脚本配置文件
+|---------package.json                    npm初始文件
+|---------README.md                       项目说明文件
+|---------webpack.config.babel.js         项目编译打包配置文件
+|---------.eslintrc                       js代码规范检测配置文件
+|---------.babelrc                        es6转换
+|---------.gitignore                      上传git时，指定忽略上传文件的配置文件
+``````
+
+## 注意事项
+* 每次启动项目后，都需重新打开微信开发者工具
---
+++ webpack.config.babel.js
@@ -0,0 +1,185 @@
+/**
+ * webpack配置文件
+ */
+import path, {
+  resolve
+} from 'path';
+import { DefinePlugin, EnvironmentPlugin } from 'webpack';
+import webpack from 'webpack';
+import WXAppWebpackPlugin from 'wxapp-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import uglifyPlugin from 'uglifyjs-webpack-plugin';
+
+const { NODE_ENV } = process.env;
+const environment = NODE_ENV;
+const srcDir = resolve('src');
+const relativeFileLoader = (ext = '[ext]') => {
+  return {
+    loader: 'file-loader',
+    options: {
+      useRelativePath: true,
+      name: `[name].${ext}`,
+      context: srcDir
+    }
+  };
+};
+
+export default {
+  entry: {
+    app: [
+      `es6-promise/dist/es6-promise.auto${JSON.stringify(environment) === 'development' ? '.min' : ''}.js`,
+      './src/app.js',
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    publicPath: '/',
+    path: resolve('dist'),
+  },
+  resolve: {
+    modules: [resolve(__dirname, 'src'), 'node_modules'],
+    alias: {
+      'src': resolve(__dirname, './src'),
+      'components': resolve(__dirname, './src/components'),
+      'images': resolve(__dirname, './src/images'),
+      'sass': resolve(__dirname, './src/sass')
+    }
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        include: /src/,
+        use: [
+          'babel-loader'
+        ].filter(Boolean)
+      },
+      {
+        test: /\.js$/,
+        include: /src/,
+        loader: 'eslint-loader',
+        options: {
+          formatter: require('eslint-friendly-formatter'),
+          emitWarning: true
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        include: /src/,
+        use: relativeFileLoader()
+      },
+      {
+        test: /\.json$/,
+        include: /src/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
+          {
+            loader: 'webpack-comment-remover-loader',
+            options: {
+              includePaths: [
+                resolve('src'),
+              ],
+            },
+          },
+        ].filter(Boolean)
+      },
+      {
+        test: /\.(scss|wxss)$/,
+        include: /src/,
+        use: [
+          relativeFileLoader('wxss'),
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [resolve('src', 'styles'), srcDir]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(wxml|axml|xml)$/,
+        include: /src/,
+        use: [
+          relativeFileLoader('wxml'),
+          {
+            loader: 'wxml-loader',
+            options: {
+              root: srcDir,
+              enforceRelativePath: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.wxs$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: [
+          relativeFileLoader('wxs'),
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: [
+                'es2015', 
+                'stage-0'
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new EnvironmentPlugin({
+      NODE_ENV: 'development'
+    }),
+    new DefinePlugin({
+      ENVIRONMENT: JSON.stringify(environment)
+    }),
+    new CopyWebpackPlugin([{
+        from: __dirname + '/src/images',
+        to: __dirname + '/dist/images'
+      },
+      {
+        from: __dirname + '/src/components',
+        to: __dirname + '/dist/components',
+        ignore: ['*/*.xml']
+      },
+      {
+        from: '**/*.wxml',
+        to: 'pages',
+        context: path.join(__dirname, 'src/pages')
+      },
+      {
+        from: __dirname + '/src/wxs',
+        to: __dirname + '/dist/wxs'
+      }
+    ], {
+      ignore: [
+        '**/*.scss',
+        '**/*.js'
+      ]
+    }),
+    new WXAppWebpackPlugin(),
+    new uglifyPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_debugger: NODE_ENV === 'development' ? false : true,
+        drop_console: NODE_ENV === 'development' ? false : true
+      }
+    })
+  ],
+  watchOptions: {
+    ignored: /dist|manifest/,
+    aggregateTimeout: 300,
+  }
+};
